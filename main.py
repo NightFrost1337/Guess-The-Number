ac323a155b91e32a95096e368737f1a050a22d78
NightFrost1337 <90924656+NightFrost1337@users.noreply.github.com>
2022-02-06 16:01:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+import os
+
+x = 0
+os.system('clear')
+num = random.randint(0,100)
+print ("Guess The Number")
+print ("I'll choose a number and u should guess it!'")
+print ("Developped by NightFrost#0001")
+input('Press Space To Start!')
+os.system("clear")
+n = int(input('guess: '))
+os.system("clear")
+
+while n != num:
+	if n < num:
+	 x=x+1
+	 print (n,' ==> guess a bigger number  -    ',x,'  Guesse(s)')
+	 n = int(input('guess: '))
+	 os.system("clear")
+	if n > num:
+	 x=x+1
+	 print (n,' ==> guess a smaller number  -    ',x,'  Guesse(s)')
+	 n = int(input('guess: '))
+	 os.system("clear")
+os.system("clear")
+x=x+1
+print (n,' ==> GG You Won!    number was: ',num,'   -    with ',x,' guesse(s)')
